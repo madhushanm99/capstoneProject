a3d931d60159e413ec7524a078a20836d9174d73
Dinithi <67262436+madhushanm99@users.noreply.github.com>
2023-12-03 12:33:01 +0530

    Add Footer an more css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,44 @@
-// Footer.js
-import React from 'react';
+import React from "react"
+import small_logo from "../assets/image_assets/logo_small.png"
 
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2023 Little Lemon. All rights reserved.</p>
-      <p>Address: 123 Lemon Street, Lemonville</p>
-      <p>Email: info@littlelemon.com</p>
-    </footer>
-  );
+const Footer = () => {
+    return(
+        <footer className="">
+            <section>
+            <div className="company-info">
+                <img src={small_logo} alt=""/>
+                <p>We are a family owned Mediterraneran restaurant, focused on traditional recipes servred with a modern twist.</p>
+            </div>
+            <div className="navigation">
+                <h3>Important Links</h3>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Menu</a></li>
+                <li><a href="/">Reservations</a></li>
+                <li><a href="/">Order Online</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
+            </div>
+            <div className="contact">
+                <h3>Contact</h3>
+            <ul>
+            <li>Address: <br/> 123 Town Street, Chicago</li>
+                <li>Phone: <br/> +00 123 456 789</li>
+                <li>Email: <br/> little@lemon.com</li>
+            </ul>
+            </div>
+            <div className="socialLink">
+                <h3>Social Media Links</h3>
+            <ul>
+                <li><a href="/">Facebook</a></li>
+                <li><a href="/">Instagram</a></li>
+                <li><a href="/">Twitter</a></li>
+            </ul>
+            </div>
+            </section>
+        </footer>
+    )
 }
 
 export default Footer;
